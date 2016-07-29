030dcc58cc20e1fc7326df32ce2501ad58f1c13d
who1sth1s <ksjlike69@naver.com>
2016-07-29 11:44:48 +0900

    Add all_page

--- blog/views.py
+++ blog/views.py
@@ -12,11 +12,19 @@ def index(request, page=1):
     end_pos = start_pos + per_page
     entries = Entries.objects.all().order_by('-created')[start_pos:end_pos]
     page_title = '블로그 글 목록 화면'
+    all_pos = Entries.objects.count()
+    if all_pos % 5 == 0:
+        all_page = int(all_pos/5)
+    else:
+        all_page = str(int(all_pos/5) + 1)
+    print(all_page, all_pos)
+
     tpl = loader.get_template('list.html')
     ctx = Context({
         'page_title': page_title,
         'entries': entries,
-        'current_page': page
+        'current_page': page,
+        'all_page': all_page
     })
     return HttpResponse(tpl.render(ctx))
 
@@ -24,9 +32,6 @@ def index(request, page=1):
 def read(request, entry_id=None):
     page_title = '글 읽기 화면'
     current_entry = Entries.objects.get(id=int(entry_id))
-    prev_entry = current_entry.get_previous_by_created()
-    next_entry = current_entry.get_next_by_created()
-
-
-    return HttpResponse('Hello Universe. [%d]nd Post is [%s]!! :)' %
-                        (current_entry.id, current_entry.Title.encode('utf-8')))
+    pre_entry = current_entry.get_previous_by_created()
+    nxt_entry = current_entry.get_next_by_created()
+    return HttpResponse(page_title)
